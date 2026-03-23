e0974e6c6ef47d50d6a41334eff5b516d499956d
Arise -D Nexus <divinewisdomorima@gmail.com>
2026-03-23 19:50:56 +0100

    Mar/23/2026-Update

--- Excercise5.py
+++ Excercise5.py
@@ -1,5 +1,5 @@
-
-        
+     
+    #  Exercise 5.1: FizzBuzz for AI   
 def main2():
     for n in range(21):
         if n%3 == 0 and n%5 == 0:
@@ -11,6 +11,7 @@ def main2():
         else:
             print(n) 
                
+    #    Exercise 5.2: Early Stopping Simulator
 def main():
     loss = 10.0
     print("Simulating Model Training")
